e48a474f5dfd02255e928f65e78c0919058615d7
Jared <jmezzbmxer@live.ca>
2018-01-08 01:31:21 -0500

    Portfolio + Contact
    Portfolio structure is done, contact form is built but not functional

--- homeJS.js
+++ homeJS.js
@@ -2,3 +2,9 @@ $('.navTrigger').click(function() {
     $('.navHidden').fadeToggle();
     $('.arrow').fadeToggle();
 })
+
+$('.submitButton').click(function() {
+    $(this).addClass('afterButton');
+    $(this).text("Thanks, Ill get back to you soon!")
+    $(this).attr('disabled', true)
+})
