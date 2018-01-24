c3fceb6d24681fe32c37a5569701b9958a6303f9
Jared <jmezzbmxer@live.ca>
2018-01-24 15:09:07 -0500

    Styled header

--- homeJS.js
+++ homeJS.js
@@ -1,10 +1,23 @@
 $('.navTrigger').click(function() {
-    $('.navHidden').fadeToggle();
-    $('.arrow').fadeToggle();
-})
+    if ($('.navTrigger').hasClass('col-md-12')){
+        $(this).addClass('fakeIt');
+        $(this).toggleClass('col-md-12');
+        $(this).toggleClass('col-md-4');
+        $('.navHidden').fadeToggle();
+        $(this).removeClass('fakeIt');
+    } else {
+        $('.navTrigger').toggleClass('nameEmph');
+        $(this).addClass('fakeIt');
+        $('.navHidden').fadeToggle();
+        $(this).toggleClass('col-md-4');
+        $('.navTrigger').toggleClass('col-md-12')
+        $(this).removeClass('fakeIt');
+        $(this).toggleClass('nameEmph');
+    }
+});
 
 $('.submitButton').click(function() {
     $(this).addClass('afterButton');
-    $(this).text("Thanks, Ill get back to you soon!")
-    $(this).attr('disabled', true)
-})
+    $(this).text("Thanks, Ill get back to you soon!");
+    $(this).attr('disabled', true);
+});
