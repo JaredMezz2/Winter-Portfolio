63fbcf924a7b75c6267702923d4c36b45f402210
Jared <Jared.Mezzatesta@gmail.com>
2019-01-06 15:21:54 -0500

    Fixed some formatting & made more mobile friendly

---
+++ README.md
@@ -0,0 +1,2 @@
+My Personal Website, somewhat of a portfolio despite not having much to show off just yet.
+Winter themed because Christmas had me in the spirit. 
---
+++ homeJS.js
@@ -0,0 +1,18 @@
+$('.navTrigger').click(function() {
+    if ($('.navTrigger').hasClass('col-sm-12')){
+        $(this).removeClass('col-sm-12');
+        $(this).addClass('col-sm-4');
+        $('.navHidden').fadeIn();
+    } else {
+        $('.navHidden').fadeOut('slow', function(){
+            $('.navTrigger').removeClass('col-sm-4');
+            $('.navTrigger').addClass('col-sm-12');
+        });
+    }
+});
+
+$('.submitButton').click(function() {
+    $(this).addClass('afterButton');
+    $(this).text("Thanks, Ill get back to you soon!");
+    $(this).attr('disabled', true);
+});
